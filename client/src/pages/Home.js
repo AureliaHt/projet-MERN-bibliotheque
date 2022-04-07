310b0910be779bca67969576707c0d2154a1ac53
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-04-07 16:50:18 +0200

    mise en place du front

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import LogForm from "../components/logForm/LogForm";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="log-container">
+                < LogForm />
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ client/src/pages/UserProfil.js
@@ -0,0 +1,9 @@
+const UserProfil = () => {
+    return (
+        <div>
+            <h1>Bienvenue sur ton compte !</h1>
+        </div>
+    );
+};
+
+export default UserProfil;
